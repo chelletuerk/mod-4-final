e8ca79ffb1a7f0c3a49fe32f4264f7600df4f0f8
chelle tuerk <michelletuerk@gmail.com>
2017-04-13 12:21:23 -0600

    live on heroku

--- public/index.js
+++ public/index.js
@@ -17,9 +17,24 @@ const renderItems = (data) => {
   data.map(obj => {
     $('.list').append(`
       <div id=${obj.id}><br>
-        ${obj.name}<br>
-        ${obj.reason}<br>
-        ${obj.cleanliness}
+        <h2>Gargabe Item:</h2> <h3>${obj.name}</h3>
+        <h2>Reason to hang onto this gem:</h2> <h3>${obj.reason}</h3>
+        <h2>How filthy is this thing?:</h2> <h3>${obj.cleanliness}</h3>
       </div>`)
   })
+}
+
+const postItem = (item) => {
+  fetch(`/api/v1/items`, {
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    method: 'POST',
+    body: JSON.stringify({ itemId: item })
+  })
+  .then(response => response.json()).then(data => {
+    currentItems = data
+    renderItems([data[data.length-1]])
+  })
+  .catch(err => 'err')
 }
