55033605ca75dbb4ecbfae679c8a5a88c8b70b8f
chelle tuerk <michelletuerk@gmail.com>
2017-04-13 18:02:07 -0600

    sort functionality

--- server.js
+++ server.js
@@ -31,6 +31,22 @@ app.get('/api/v1/items', (request, response) => {
   })
 })
 
+app.get('/api/v1/sorted_items', (request, response) => {
+  database('items').select()
+  .then((items) => {
+    const sortedItems = items.sort((a, b) => {
+      const aCode = a.name.split('')[0].charCodeAt()
+      const bCode = b.name.split('')[0].charCodeAt()
+      return aCode > bCode
+    })
+
+    response.status(200).json(sortedItems)
+  })
+  .catch((error) => {
+    response.status(404).json({'Response 404': 'Not Found'})
+  })
+})
+
 app.post('/api/v1/items', (request, response) => {
   const { name, reason, cleanliness } = request.body
   const item = { name, reason, cleanliness }
@@ -45,16 +61,17 @@ app.post('/api/v1/items', (request, response) => {
       });
   })
 })
-
-
-
-
-
-
-
-
-
-
+app.get('/api/v1/items/:id', (request, response)=> {
+  const { id } = request.params
+    database('items').where('id', id).select().del()
+    .then(function(count) {
+      if (count === 0) {
+        response.status(422).json({'Response 422': 'Unprocessable Entity'})
+      } else {
+        response.status(200).json({'Response 200': 'OK' })
+      }
+    })
+})
 
 app.listen(app.get('port'), () => {
   console.log(`Running on ${app.get('port')}`)
